7a931c1d72185c50b55a18d2d7876d056ed4cb25
Atiwat Seenark <um_oom@hotmail.com>
2025-09-12 11:27:02 +0700

    add deploy workflow

--- .github/actions/deploy-to-surge/index.ts
+++ .github/actions/deploy-to-surge/index.ts
@@ -1,13 +1,24 @@
 import * as core from "@actions/core"
+import * as exec from "@actions/exec"
+
+// surge dist --token d2f81d98ab5f03e8da3fb6848c6a5f15 --login um_oom@hotmail. --domain gh-action-tanstack.surge.sh
 
 
 async function main() {
 
 
+  // get inputs values
   const domain = core.getInput("domain", { required: true, trimWhitespace: true })
   const distFolder = core.getInput("dist-folder")
+  const token = core.getInput("token", { required: true, trimWhitespace: true })
+  const email = core.getInput("email", { required: true, trimWhitespace: true })
 
+  const command = `surge ${distFolder} --token ${token} --login ${email} --domain ${domain}`
 
+  // deploy to surge
+  const exitCode = await exec.exec(command)
+
+  return exitCode
 }
 
 main()
