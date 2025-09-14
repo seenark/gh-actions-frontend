fced1bd2001ed58aa42cd7a7507eee6993b7aff2
Atiwat Seenark <um_oom@hotmail.com>
2025-09-14 23:40:36 +0700

    fix inputs

--- .github/actions/deploy-to-surge-ts/index.ts
+++ .github/actions/deploy-to-surge-ts/index.ts
@@ -5,14 +5,14 @@ async function main() {
 
   // get inputs values
   const domain = Bun.env.INPUT_DOMAIN
-  const distFolder = Bun.env["INPUT_DIST-FOLDER"]
+  const distFolder = Bun.env["INPUT_DIST_FOLDER"]
   const token = Bun.env.INPUT_TOKEN
   const email = Bun.env.INPUT_EMAIL
 
-  const command = `bun x surge ${distFolder} --token ${token} --login ${email} --domain ${domain}`
   // const command = `npx surge ${distFolder} --domain ${domain}`
+  console.log({ domain, distFolder, token, email })
 
-  const exitCode = await $`${command}`
+  const exitCode = await $`surge ${distFolder} --token ${token} --login ${email} --domain ${domain}`
   const websiteUrl = `https://${domain}`
   console.log(`::set-output name=website-url::${websiteUrl}`)
 
@@ -23,3 +23,4 @@ main()
 
 
 
+// INPUT_DOMAIN="gh-action-tanstack.surge.sh" INPUT_DIST_FOLDER="dist" INPUT_TOKEN="d2f81d98ab5f03e8da3fb6848c6a5f15"  INPUT_EMAIL="um_oom@hotmail.com" bun index.ts
