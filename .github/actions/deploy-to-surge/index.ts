e5fb7fc0b68dd3af722506e23e10734b31c59bcf
Atiwat Seenark <um_oom@hotmail.com>
2025-09-14 15:23:29 +0700

    use env for surge token

--- .github/actions/deploy-to-surge/index.ts
+++ .github/actions/deploy-to-surge/index.ts
@@ -8,10 +8,11 @@ async function main() {
   // get inputs values
   const domain = core.getInput("domain", { required: true, trimWhitespace: true })
   const distFolder = core.getInput("dist-folder")
-  const token = core.getInput("token", { required: true, trimWhitespace: true })
-  const email = core.getInput("email", { required: true, trimWhitespace: true })
+  // const token = core.getInput("token", { required: true, trimWhitespace: true })
+  // const email = core.getInput("email", { required: true, trimWhitespace: true })
 
-  const command = `npx surge ${distFolder} --token ${token} --login ${email} --domain ${domain}`
+  // const command = `npx surge ${distFolder} --token ${token} --login ${email} --domain ${domain}`
+  const command = `npx surge ${distFolder} --domain ${domain}`
 
   // deploy to surge
   const exitCode = await exec.exec(command)
